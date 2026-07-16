7fb804a3995ee36a76bb5452eb089d4cfc6fda1a
high-haseeb <haseebkhalidoriginal@gmail.com>
2026-07-17 02:38:21 +0500

    Update vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite"; 
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    base: '/keyboard-configurator',
     plugins: [
         tailwindcss(),
     ],
